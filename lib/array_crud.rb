5b493bc2623b368f84c8f0b8b130d0947d366936
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-02 03:41:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Mercedes", "BMW", "Subaru", "Mazda"]
+  cars = ["Mercedes", "BMW", "Subaru", "Mazda"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  cars << "Hyundai"
 end
 
 def add_element_to_start_of_array(array, element)
